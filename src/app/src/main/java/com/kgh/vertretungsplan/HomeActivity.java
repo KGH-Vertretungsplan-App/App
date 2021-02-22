1323db4f3a862d6c62732991d639ed69688f1f28
byZeroOfficial <76685126+byZeroOfficial@users.noreply.github.com>
2021-02-22 12:33:49 +0100

    added the full App-Code

---
+++ src/app/src/main/java/com/kgh/vertretungsplan/HomeActivity.java
@@ -0,0 +1,229 @@
+package com.kgh.vertretungsplan;
+
+import android.app.Activity;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.graphics.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.text.*;
+import android.widget.LinearLayout;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.content.Intent;
+import android.net.Uri;
+import android.content.SharedPreferences;
+import android.view.View;
+
+public class HomeActivity extends Activity {
+	
+	
+	private LinearLayout linear1;
+	private WebView webview1;
+	private Button button3;
+	
+	private Intent i = new Intent();
+	private SharedPreferences Download;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.home);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		webview1 = (WebView) findViewById(R.id.webview1);
+		webview1.getSettings().setJavaScriptEnabled(true);
+		webview1.getSettings().setSupportZoom(true);
+		button3 = (Button) findViewById(R.id.button3);
+		Download = getSharedPreferences("Download", Activity.MODE_PRIVATE);
+		
+		webview1.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView _param1, String _param2, Bitmap _param3) {
+				final String _url = _param2;
+				
+				super.onPageStarted(_param1, _param2, _param3);
+			}
+			
+			@Override
+			public void onPageFinished(WebView _param1, String _param2) {
+				final String _url = _param2;
+				
+				super.onPageFinished(_param1, _param2);
+			}
+		});
+		
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				i.setClass(getApplicationContext(), MainviewActivity.class);
+				startActivity(i);
+			}
+		});
+	}
+	private void initializeLogic() {
+		webview1.loadUrl("https://google.com");
+		webview1.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+		webview1.loadUrl("https://cloud.0network.de/vertretungsplan/home/2.1.0/");
+		if (Download.getString("dl-20.02.2021", "").equals("")) {
+			_webviewDialog("https://kghalle.tk/changelog/latest", "- Neues Update - Changelog", "Okay", false, "#ffffff");
+			Download.edit().putString("dl-20.02.2021", "gotya").commit();
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		
+	}
+	private void _webviewDialog (final String _URL, final String _title, final String _btn1, final boolean _setC, final String _hexC) {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this); 
+		alert.setTitle(_title);
+		
+		WebView wv = new WebView(this);
+		wv.loadUrl(_URL);
+		wv.setBackgroundColor(Color.parseColor(_hexC));
+		
+		WebSettings webSettings = wv.getSettings();
+		webSettings.setJavaScriptEnabled(true); webSettings.setUseWideViewPort(true);
+		webSettings.setLoadWithOverviewMode(true);
+		webSettings.setSupportZoom(false);
+		webSettings.setBuiltInZoomControls(true);
+		webSettings.setDisplayZoomControls(true);
+		wv.setVerticalScrollBarEnabled(true);
+		wv.setHorizontalScrollBarEnabled(true);
+		
+		wv.setDownloadListener(new DownloadListener() {
+			public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+				DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+				String cookies = CookieManager.getInstance().getCookie(url);
+				request.addRequestHeader("cookie", cookies);
+				request.addRequestHeader("User-Agent", userAgent);
+				request.setDescription("Downloading file to downloads folder...");
+				request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+				request.allowScanningByMediaScanner(); request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+				java.io.File aatv = new java.io.File(Environment.getExternalStorageDirectory().getPath() + "/downloads/");
+				
+				if(!aatv.exists()){if (!aatv.mkdirs()){ Log.e("TravellerLog ::","Problem creating Image folder");}} request.setDestinationInExternalPublicDir("/downloads/", URLUtil.guessFileName(url, contentDisposition, mimetype));
+				
+				DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+				
+				manager.enqueue(request);
+				
+				showMessage("Downloading ...");
+				
+				//Notif if success
+				
+				BroadcastReceiver onComplete = new BroadcastReceiver() {
+					
+					public void onReceive(Context ctxt, Intent intent) {
+						showMessage("Download finished !");
+						
+						unregisterReceiver(this);
+						
+					}};
+				
+				registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+				
+			}
+			
+		});
+		
+		wv.setWebViewClient(new WebViewClient() {
+			    @Override
+			    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				        view.loadUrl(url);
+				
+				        return true;
+				    }
+		});
+		
+		alert.setView(wv);
+		alert.setNegativeButton(_btn1, new DialogInterface.OnClickListener() {
+			    @Override
+			    public void onClick(DialogInterface dialog, int id) {
+				        dialog.dismiss();
+				    }
+		});
+		alert.setCancelable(_setC);
+		alert.show();
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
